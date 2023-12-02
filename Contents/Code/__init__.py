2dd4113c2b72d062fdb2427631d17a593e244570
xjasonlyu <xjasonlyu@gmail.com>
2023-12-01 23:23:28 -0500

    add

---
+++ Contents/Code/__init__.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+# plex debugging
+try:
+    import plexhints  # noqa: F401
+except ImportError:
+    pass
+else:  # the code is running outside of Plex
+    from plexhints.agent_kit import Agent, Media  # agent kit
+    from plexhints.core_kit import Core  # core kit
+    from plexhints.decorator_kit import handler, indirect, route  # decorator kit
+    from plexhints.exception_kit import Ex  # exception kit
+    from plexhints.locale_kit import Locale  # locale kit
+    from plexhints.log_kit import Log  # log kit
+    from plexhints.model_kit import Movie, VideoClip, VideoClipObject  # model kit
+    from plexhints.network_kit import HTTP  # network kit
+    from plexhints.object_kit import Callback, IndirectResponse, MediaObject, MessageContainer, MetadataItem, \
+        MetadataSearchResult, PartObject, SearchResult  # object kit
+    from plexhints.parse_kit import HTML, JSON, Plist, RSS, XML, YAML  # parse kit
+    from plexhints.prefs_kit import Prefs  # prefs kit
+    from plexhints.proxy_kit import Proxy  # proxy kit
+    from plexhints.resource_kit import Resource  # resource kit
+    from plexhints.shortcut_kit import L, E, D, R, S  # shortcut kit
+    from plexhints.util_kit import String, Util  # util kit
+
+    from plexhints.constant_kit import CACHE_1MINUTE, CACHE_1HOUR, CACHE_1DAY, CACHE_1WEEK, CACHE_1MONTH  # constant kit
+    from plexhints.constant_kit import ClientPlatforms, Protocols, OldProtocols, ServerPlatforms, ViewTypes, \
+        SummaryTextTypes, AudioCodecs, VideoCodecs, Containers, ContainerContents, \
+        StreamTypes  # constant kit, more commonly used in URL services
+
+    # extra objects
+    from plexhints.extras_kit import BehindTheScenesObject, ConcertVideoObject, DeletedSceneObject, FeaturetteObject, \
+        InterviewObject, LiveMusicVideoObject, LyricMusicVideoObject, MusicVideoObject, OtherObject, \
+        SceneOrSampleObject, ShortObject, TrailerObject
+
+
+def Start():
+    HTTP.ClearCache()
+    HTTP.CacheTime = CACHE_1DAY
+    HTTP.Headers['User-Agent'] = 'MetaTube.bundle'
+    HTTP.Headers['Accept-Encoding'] = 'gzip'
+
+
+def ValidatePrefs():
+    Log.Debug('ValidatePrefs called.')
+
+
+class MetaTubeAgent(Agent.Movies):
+    name = 'MetaTube'
+    languages = [Locale.Language.English]
+    primary_provider = True
+    accepts_from = ['com.plexapp.agents.localmedia', 'com.plexapp.agents.lambda', 'com.plexapp.agents.xbmcnfo']
+    contributes_to = ['com.plexapp.agents.none']
+
+    def search(self, results, media, lang, manual=False):
+        pass
+
+    def update(self, metadata, media, lang, force=False):
+        pass
