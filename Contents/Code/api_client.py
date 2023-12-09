8abb2fa8a5ee5aa195eb543422025e1c12a8777e
xjasonlyu <xjasonlyu@gmail.com>
2023-12-08 23:08:28 -0500

    fix api client with _

--- Contents/Code/api_client.py
+++ Contents/Code/api_client.py
@@ -22,31 +22,28 @@ class APIError(Exception):
     """An API error occurred."""
 
 
-class APIClient:
-    _ACTOR_INFO_API = '/v1/actors'
-    _MOVIE_INFO_API = '/v1/movies'
-    _ACTOR_SEARCH_API = '/v1/actors/search'
-    _MOVIE_SEARCH_API = '/v1/movies/search'
-    _PRIMARY_IMAGE_API = '/v1/images/primary'
-    _THUMB_IMAGE_API = '/v1/images/thumb'
-    _BACKDROP_IMAGE_API = '/v1/images/backdrop'
-    _TRANSLATE_API = '/v1/translate'
+class APIClient(object):
+    ACTOR_INFO_API = '/v1/actors'
+    MOVIE_INFO_API = '/v1/movies'
+    ACTOR_SEARCH_API = '/v1/actors/search'
+    MOVIE_SEARCH_API = '/v1/movies/search'
+    PRIMARY_IMAGE_API = '/v1/images/primary'
+    THUMB_IMAGE_API = '/v1/images/thumb'
+    BACKDROP_IMAGE_API = '/v1/images/backdrop'
+    TRANSLATE_API = '/v1/translate'
 
     def __init__(self):
         self.session = Session()
 
-    def __del__(self):
-        self.session.close()
-
     @staticmethod
-    def _prepare_url(*paths, **params):
+    def prepare_url(*paths, **params):
         req = PreparedRequest()
         req.prepare_url(
             url=urljoin(Prefs[KEY_API_SERVER], pathjoin(*paths)),
             params=params)
         return req.url
 
-    def _get_data(self, url, require_auth=False):
+    def get_data(self, url, require_auth=False):
         headers = {
             'Accept': 'application/json',
             'User-Agent': DEFAULT_USER_AGENT
@@ -68,50 +65,50 @@ class APIClient:
             return data
 
     def search_actor(self, q, provider=None, fallback=None):
-        return self._get_data(
-            url=self._prepare_url(
-                self._ACTOR_SEARCH_API,
+        return self.get_data(
+            url=self.prepare_url(
+                self.ACTOR_SEARCH_API,
                 q=q, provider=provider, fallback=fallback),
             require_auth=True)
 
     def search_movie(self, q, provider=None, fallback=None):
-        return self._get_data(
-            url=self._prepare_url(
-                self._MOVIE_SEARCH_API,
+        return self.get_data(
+            url=self.prepare_url(
+                self.MOVIE_SEARCH_API,
                 q=q, provider=provider, fallback=fallback),
             require_auth=True)
 
     def get_actor_info(self, provider, id, lazy=None):
-        return self._get_data(
-            url=self._prepare_url(
-                self._ACTOR_INFO_API, provider, id,
+        return self.get_data(
+            url=self.prepare_url(
+                self.ACTOR_INFO_API, provider, id,
                 lazy=lazy),
             require_auth=True)
 
     def get_movie_info(self, provider, id, lazy=None):
-        return self._get_data(
-            url=self._prepare_url(
-                self._MOVIE_INFO_API, provider, id,
+        return self.get_data(
+            url=self.prepare_url(
+                self.MOVIE_INFO_API, provider, id,
                 lazy=lazy),
             require_auth=True)
 
     def get_primary_image_url(self, provider, id, ratio=None, pos=None):
-        return self._prepare_url(
-            self._PRIMARY_IMAGE_API, provider, id,
+        return self.prepare_url(
+            self.PRIMARY_IMAGE_API, provider, id,
             ratio=ratio, pos=pos)
 
     def get_thumb_image_url(self, provider, id):
-        return self._prepare_url(
-            self._THUMB_IMAGE_API, provider, id)
+        return self.prepare_url(
+            self.THUMB_IMAGE_API, provider, id)
 
     def get_backdrop_image_url(self, provider, id):
-        return self._prepare_url(
-            self._BACKDROP_IMAGE_API, provider, id)
+        return self.prepare_url(
+            self.BACKDROP_IMAGE_API, provider, id)
 
     def translate(self, q, to, engine, **params):
-        return self._get_data(
-            url=self._prepare_url(
-                self._TRANSLATE_API,
+        return self.get_data(
+            url=self.prepare_url(
+                self.TRANSLATE_API,
                 q=q, to=to, engine=engine, **params),
             require_auth=False)
 
