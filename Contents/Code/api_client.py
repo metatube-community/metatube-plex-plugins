5e62388ec7c1e4f1fb6e19f9eb548592e2f487e0
xjasonlyu <xjasonlyu@gmail.com>
2023-12-10 10:08:31 -0500

    style spotless

--- Contents/Code/api_client.py
+++ Contents/Code/api_client.py
@@ -25,8 +25,7 @@ def parse_date(s):
     # noinspection PyBroadException
     try:
         return datetime.strptime(
-            findall(r'\d{4}-\d{2}-\d{2}', s)[0],
-            '%Y-%m-%d')
+            findall(r'\d{4}-\d{2}-\d{2}', s)[0], '%Y-%m-%d')
     except:
         return datetime(1, 1, 1)
 
