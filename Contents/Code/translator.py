23bef544f4cd19fc5d594c531c512930b810296d
xjasonlyu <xjasonlyu@gmail.com>
2023-12-14 15:40:33 -0500

    l

--- Contents/Code/translator.py
+++ Contents/Code/translator.py
@@ -45,7 +45,7 @@ def translate_text(text, lang, fallback=None):
                      .format(lang=lang, forced_lang=forced_lang))
             lang = forced_lang
 
-        # Limit translate request rate to 1 rps.
+        # limit translate request rate to 1 rps.
         time.sleep(1.0)
 
         try:
